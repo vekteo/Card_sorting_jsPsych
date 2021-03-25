a79609884ed20d7f680dce6fba45fd5308f29798
Teodóra Vékony <45353438+vekteo@users.noreply.github.com>
2021-03-25 16:30:38 +0100

    add new languages

--- languages.js
+++ languages.js
@@ -4,15 +4,15 @@ const en = {
         clickNext: "Click on <strong>Next</strong> to continue!"
     },
     instruction: {
-        fourCards: "In this task, four cards will appear on the screen. Each card has a different <strong>number</strong>, <strong>color</strong>, and <strong>shape</strong>.",
+        fourCards: "In this task, four cards will appear on the screen. Each card has figures that differs in <strong>number</strong>, <strong>color</strong>, and <strong>shape</strong>.",
         newCard: "From time to time, you will receive a new card, and your task will be to try to determine which of the four cards the new card belongs to.",
         clickCard: "You can categorize the new card by clicking on one of the four cards.",
-        rule: "The correct answer depends upon <strong>a rule</strong>, but you will not know what the rule is. But we will tell you on each trial whether or not you were correct.",
+        rule: "The correct answer depends upon <strong>a rule</strong>. We will not tell you what the rule is. However, we will tell you if your answer was correct or not.",
     },
     instruction2: {
         ruleChange: "The <strong>rule may change</strong> during the task.",
         ruleChange2: "When the rule changes, you should figure out what the rule is as quickly as possible.",
-        clickNext: "If the instructions are clear, click on the <strong>Next</strong> button to start the task!",
+        clickNext: "If the instructions are clear, click on the <strong>Next</strong> key to start the task!",
 
     },
     task: {
@@ -71,5 +71,119 @@ const hu = {
     feedback: {
         correct: "Helyes!",
         wrong: "Helytelen!"
+    }
+}
+
+const es = {
+    welcomePage: {
+        welcome: "Bienvenid@!",
+        clickNext: "Haz clic en <strong>Siguiente</strong> para continuar!"
+    },
+    instruction: {
+        fourCards: "En esta tarea, cuatro tarjetas aparecerán en pantalla. Cada tarjeta tiene íconos que difieren en cuanto a <strong>cantidad</strong>, <strong>color</strong>, y <strong>forma</strong>.",
+        newCard: "Verás aparecer una quinta tarjeta que deberás clasificar en conjunto a una de las cuatro tarjetas iniciales en función de su <strong>cantidad</strong>, <strong>color</strong>, o <strong>forma</strong>.",
+        clickCard: "Para categorizar la tarjeta nueva haz clic en una de las cuatro tarjetas.",
+        rule: "La respuesta correcta dependerá de <strong>una regla</strong>. Sin embargo, la regla no te será indicada. No obstante, te diremos a cada vez si tu respuesta es correcta o incorrecta.",
+    },
+    instruction2: {
+        ruleChange: "Es posible <strong>que la regla cambie</strong> durante la tarea.",
+        ruleChange2: "Cuando la regla cambie deberás adaparte lo más rápido que puedas.",
+        clickNext: "Si comprendiste las instrucciones, haz clic en <strong>Siguiente</strong>!",
+
+    },
+    task: {
+        instruction: "¿A cuál de las cuatro tarjetas pertenece la nueva tarjeta? Haz clic sobre una de las cuatro tarjetas!"
+    },
+    end:  {
+        end: "Fin de la tarea.",
+        thankYou: "Gracias por haber participado!",
+    },
+    button: {
+        next: "Siguiente",
+        previous: "Atrás"
+    },
+    fullscreen: {
+        fullscreenText: "El experimento pasará a pantalla completa al cliquear en Continuar.",
+        fullscreenButton: "Continuar"
+    },
+    feedback: {
+        correct: "Correcto!",
+        wrong: "Incorrecto!"
+    }
+}
+
+const fr = {
+    welcomePage: {
+        welcome: "Bienvenue!",
+        clickNext: "Cliquez sur <strong>Suivant</strong> pour continuer!"
+    },
+    instruction: {
+        fourCards: "Dans cette tâche, quatre cartes vont apparaître à l'écran. Chaque carte a <strong>un chiffre différent</strong>, <strong>une couleur différente</strong>, et <strong>une forme différente</strong>.",
+        newCard: "Vous aller recevoir une nouvelle carte, et votre tâche consistera à essayer de déterminer à laquelle des quatre cartes appartient la nouvelle carte.",
+        clickCard: "Vous classerez la nouvelle carte en cliquant sur l'une des quatre cartes.",
+        rule: "La bonne réponse dépend <strong>d'une règle</strong>. Nous ne vous dirons pas quelle est cette règle. Cependant, nous vous dirons si votre réponse est correcte ou non.",
+    },
+    instruction2: {
+        ruleChange: "La <strong>règle peut changer</strong> durant la tâche.",
+        ruleChange2: "Quand la règle change, vous devez trouver la nouvelle règle le plus rapidement possible.",
+        clickNext: "Si les instructions sont claires, cliquez sur le bouton <strong>Suivant</strong> pour démarrer la tâche!",
+
+    },
+    task: {
+        instruction: "À quelle carte la nouvelle carte appartient-elle ? Cliquez sur l'une des quatre cartes en haut!"
+    },
+    end:  {
+        end: "Fin de la tâche.",
+        thankYou: "Merci de votre participation!",
+    },
+    button: {
+        next: "Suivant",
+        previous: "Précédent"
+    },
+    fullscreen: {
+        fullscreenText: "L'expérience passera en mode plein écran lorsque vous appuierez sur le bouton ci-dessous.",
+        fullscreenButton: "Continuer"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Faux!"
+    }
+}
+
+const pt = {
+    welcomePage: {
+        welcome: "Bem-vindo!",
+        clickNext: "Clique em <strong>Próximo</strong> para continuar!"
+    },
+    instruction: {
+        fourCards: "Nessa tarefa, quatro cartas vão aparecer na tela. Cada carta tem formas que variam em <strong>número</strong>, <strong>cor</strong>, e <strong>forma</strong>.",
+        newCard: "De tempos em tempos, você receberá uma nova carta, e sua tarefa será determinar em qual das quatro cartas a nova carta pertence.",
+        clickCard: "Você pode categorizar a nova carta clicando em uma das quatro cartas.",
+        rule: "A resposta correta depende de <strong>uma regra</strong>. Não vamos te contar qual é a regra. Entretanto nós​ te diremos se sua resposta está correta ou não.",
+    },
+    instruction2: {
+        ruleChange: "A <strong>regra pode mudar</strong> durante a tarefa.",
+        ruleChange2: "Quando a regra muda, voce deve descobrir qual é a nova regra o mais rápido possível.",
+        clickNext: "Se as instruções estão claras, clique em <strong>Próximo</strong> para iniciar a sessão!",
+
+    },
+    task: {
+        instruction: "Em qual carta a nova carta pertence? Clique em uma das quatro cartas no topo!"
+    },
+    end:  {
+        end: "Fim da tarefa.",
+        thankYou: "Muito obrigado por participar!",
+    },
+    button: {
+        next: "Próximo",
+        previous: "Anterior"
+    },
+    fullscreen: {
+        fullscreenText: "O experimento irá passar para o modo tela cheia quando você clicar no botão abaixo.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correto!",
+        wrong: "Errado!"
     }
 }
